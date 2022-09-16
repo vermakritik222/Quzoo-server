b6059e11eb76e47713ffde31309782ca4b27b147
kritik <kritikverma2002@gmail.com>
2022-09-16 22:05:16 +0530

    color changed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(universalMiddleware.sendTimeStamp);
 // cors policies
 const coresOptions = {
     credentials: true,
-    origin: ['http://localhost:3000/', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
 };
 app.use(cors(coresOptions));
 
